c4d13830e22c2922fd4745e85b737cc25c6af8d9
victor costa <joaovictorf3@gmail.com>
2019-01-23 11:18:29 -0300

    fixed erros

--- miner/extract_champ_tags.py
+++ miner/extract_champ_tags.py
@@ -1,17 +1,17 @@
 import json
 import csv
 arqName = "championFull"
-arq = open(arqName+".json", "r")
+arq = open('../datasets/'+arqName+".json", "r")
 jsonStr = arq.read()
 dicJson = json.loads(jsonStr)
 champions = dicJson["data"].keys()
 filterDic = {}
 for champ in champions:
     champData = dicJson["data"][champ]
-    filterDic[champ] = {"champion_Name": champ,
-                        "tags": champData["tags"], "stats": champData["stats"]}
+    filterDic[champ] = {"champion": champ,
+                        "tags": champData["tags"]}
 
-with open(arqName+'.csv', 'w') as f:  # Just use 'w' mode in 3.x
+with open('../treasures/'+arqName+'.csv', 'w') as f:  # Just use 'w' mode in 3.x
     w = csv.DictWriter(f, filterDic["Aatrox"].keys())
     w.writeheader()
     for key in filterDic.keys():
